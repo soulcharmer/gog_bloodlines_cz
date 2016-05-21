ada6f98ba387cc6ffe32c5edd9c80fa2b3be7fc2
Charmer <soulcharmer@gmail.com>
2016-05-21 22:01:25 +0200

    pridan odhackovavac

---
+++ ascii_dump.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in `find cestina ! -wholename "*/resource*" ! -wholename "*/materials*"`; do
+
+	if [ -d $i ]
+		then
+			echo "directory : ${i//cestina/cestina_ascii}"
+			mkdir ${i//cestina/cestina_ascii} 2>/dev/null
+		else
+			echo "file : $i"
+		if file $i | grep UTF-8 1>/dev/null
+		then
+			cstocs utf8 ascii $i > ${i//cestina/cestina_ascii}
+		else
+			cstocs 1250 ascii $i > ${i//cestina/cestina_ascii}
+		fi
+	fi	
+done

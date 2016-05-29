055d2996205b735e440b906c7ea137ae1400fce4
Charmer <soulcharmer@gmail.com>
2016-05-30 01:06:53 +0200

    opravena chyba neescapovani mezer v cestach

--- ascii_dump.sh
+++ ascii_dump.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 for i in `find cestina ! -wholename "*/resource*" ! -wholename "*/materials*"`; do
 
-	if [ -d $i ]
+	if [ -d "$i" ]
 		then
 			echo "directory : ${i//cestina/cestina_ascii}"
-			mkdir ${i//cestina/cestina_ascii} 2>/dev/null
+			mkdir "${i//cestina/cestina_ascii}" 2>/dev/null
 		else
 			echo "file : $i"
-		if file $i | grep UTF-8 1>/dev/null
+		if file "$i" | grep UTF-8 1>/dev/null
 		then
-			cstocs utf8 ascii $i > ${i//cestina/cestina_ascii}
+			cstocs utf8 ascii "$i" > "${i//cestina/cestina_ascii}"
 		else
-			cstocs 1250 ascii $i > ${i//cestina/cestina_ascii}
+			cstocs 1250 ascii "$i" > "${i//cestina/cestina_ascii}"
 		fi
 	fi	
 done
+
+IFS=$SAVEIFS

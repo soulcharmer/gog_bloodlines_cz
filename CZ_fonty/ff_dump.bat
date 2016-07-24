8557d39d75136894fc574988b956afa8a83f0653
miracleflame <miracle.flame@gmail.com>
2016-07-24 15:52:56 +0200

    CZ font update 1

---
+++ CZ_fonty/ff_dump.bat
@@ -0,0 +1,94 @@
+@echo off
+echo Š
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\*" "Š" "Â"
+echo š
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\*" "š" "â"
+echo 
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\*" "" "Ë"
+echo 
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\*" "" "ë"
+echo 
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\*" "" "Î"
+echo 
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\*" "" "î"
+echo È
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "È" "Ç"
+echo è
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "è" "ç"
+echo Ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "Ø" "Ä"
+echo ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "ø" "ä"
+echo Ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "Ò" "×"
+echo ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "ò" "÷"
+echo Ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "Ù" "Ô"
+echo ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\dlg\*" "ù" "ô"
+echo È
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "È" "Ç"
+echo è
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "è" "ç"
+echo Ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "Ø" "Ä"
+echo ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "ø" "ä"
+echo Ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "Ò" "×"
+echo ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "ò" "÷"
+echo Ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "Ù" "Ô"
+echo ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\resource\*" "ù" "ô"
+echo È
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "È" "Ç"
+echo è
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "è" "ç"
+echo Ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "Ø" "Ä"
+echo ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "ø" "ä"
+echo Ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "Ò" "×"
+echo ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "ò" "÷"
+echo Ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "Ù" "Ô"
+echo ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\scripts\*" "ù" "ô"
+echo È
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "È" "Ç"
+echo è
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "è" "ç"
+echo Ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "Ø" "Ä"
+echo ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "ø" "ä"
+echo Ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "Ò" "×"
+echo ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "ò" "÷"
+echo Ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "Ù" "Ô"
+echo ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound\*" "ù" "ô"
+echo È
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "È" "Ç"
+echo è
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "è" "ç"
+echo Ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "Ø" "Ä"
+echo ø
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "ø" "ä"
+echo Ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "Ò" "×"
+echo ò
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "ò" "÷"
+echo Ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "Ù" "Ô"
+echo ù
+fart -rc "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\vdata\system\histories.txt" "ù" "ô"
+pause

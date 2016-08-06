f0f86afa1c6f85cc64275a29ec6ec841ad568279
Charmer <soulcharmer@gmail.com>
2016-08-06 23:49:11 +0200

    prvni verze translatoru pro .lip

---
+++ translate_lip.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'find'  
+require 'fileutils'
+
+folder = "./cestina/sound"
+
+Find.find(folder) do |f|  
+
+        if f.to_s =~ /.*.lip$/
+
+		puts "Processing " + f.to_s
+		FileUtils.cp(f, f.to_s.gsub(/lip/, "lip.bkp") )
+	
+		result = File.new('tmp.lip', 'w') 
+		source = File.open(f, 'rb') do |f1| 
+			while line = f1.gets
+				if line =~ /^".*"/ 
+				   plaintext = line
+				   result.puts plaintext
+				   # puts line + " " + plaintext.length.to_s
+			        elsif  line =~ /^PHRASE \w+ (\d+) .*".(.*)$/ 
+				   phrase = "PHRASE char " + plaintext.length.to_s + " " + plaintext.chop + $2.to_s
+				   result.puts phrase
+			        elsif
+				   result.puts line
+				end   
+			end
+		end
+		result.close
+		File.rename(result, f)
+	end
+end

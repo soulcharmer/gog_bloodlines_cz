a4466acba61ffc0c060566e92380a1a2a3eb9782
Charmer <soulcharmer@gmail.com>
2016-12-19 22:23:10 +0100

    uprava translate skriptu pro #103

--- translate_lip.rb
+++ translate_lip.rb
@@ -14,13 +14,17 @@ Find.find(folder) do |f|
 	
 		result = File.new('tmp.lip', 'w') 
 		source = File.open(f, 'rb') do |f1| 
+			skip_line = 0 
 			while line = f1.gets
-				if line =~ /^".*"/ 
+				if line =~ /^.*(CITOSLOVCE|BUGGED)/ 
+				   skip_line = 1
+   				   result.puts line		
+				elsif line =~ /^".*"/ 
 				   plaintext = line
 				   result.puts plaintext
 				   if line =~ /^".*" / then puts "extra space in: " + f.to_s end
 				   # puts line + " " + plaintext.length.to_s
-			        elsif  line =~ /^(PHRASE.+)(".*"[^0-9]*)([0-9].+)$/
+			        elsif  line =~ /^(PHRASE.+)(".*"[^0-9]*)([0-9].+)$/ && skip_line == 0
 			           # puts "ph: " + $1.to_s + " *** " + $2.to_s + " *** " + $3.to_s
 				   phrase = "PHRASE char " + plaintext.chop.length.to_s + " " + plaintext.chop + " " + $3.to_s
 				   result.puts phrase

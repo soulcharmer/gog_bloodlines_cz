b788a65e6f0003e9bb8121b044736d2f5e05347a
miracleflame <miracle.flame@gmail.com>
2018-10-12 12:08:31 +0200

    haven_pc #466 + ascii related tweaks

--- 1250_dump.sh
+++ 1250_dump.sh
@@ -3,7 +3,7 @@
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
-for i in `find cestina ! -wholename "*/cl_dlls*" ! -wholename "*/resource*" ! -wholename "*/materials*" ! -wholename "*/models*" ! -path *.mp3* ! -path *.vcd* ! -path *.wav* ! -path strings.txt*`; do
+for i in `find cestina ! -wholename "*/cl_dlls*" ! -wholename "*/resource*" ! -wholename "*/materials*" ! -wholename "*/models*" ! -wholename "*/vdata/hackterminals*" ! -path *.mp3* ! -path *.vcd* ! -path *.wav* ! -path strings.txt*`; do
 
 	if [ -d "$i" ]
 		then
--- CZ_fonty/FontFriendly_dump.cmd
+++ CZ_fonty/FontFriendly_dump.cmd
@@ -6,6 +6,7 @@ xcopy ..\cestina_1250\dlg\*.* ..\cestina_fontfriendly\dlg\*.* /YS
 xcopy ..\cestina_1250\scripts\*.* ..\cestina_fontfriendly\scripts\*.* /YS
 xcopy ..\cestina_1250\sound\*.* ..\cestina_fontfriendly\sound\*.* /YS
 xcopy ..\cestina_1250\vdata\*.* ..\cestina_fontfriendly\vdata\*.* /YS
+xcopy ..\cestina_ascii\vdata\hackterminals\*.* ..\cestina_fontfriendly\vdata\hackterminals\*.* /YS
 del ..\cestina_fontfriendly\*.mp3 /S /F
 del ..\cestina_fontfriendly\*.vcd /S /F
 del ..\cestina_fontfriendly\*.wav /S /F
@@ -43,6 +44,5 @@ rem "fnr.exe" --cl --dir "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfri
 "fnr.exe" --cl --dir "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound" --fileMask "*.*" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --caseSensitive --showEncoding --alwaysUseEncoding "windows-1250" --silent --find "í" --replace "`"
 "fnr.exe" --cl --dir "c:\cygwin64\home\VLK\gog_bloodlines_cz\cestina_fontfriendly\sound" --fileMask "*.*" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --caseSensitive --showEncoding --alwaysUseEncoding "windows-1250" --silent --find "Ô" --replace "^"
 
-xcopy ..\cestina_ascii\vdata\hackterminals\*.* ..\cestina_fontfriendly\vdata\hackterminals\*.* /YS
 xcopy .\resource\* ..\cestina_fontfriendly\resource\* /YS
 xcopy .\strings.txt ..\cestina_fontfriendly\vdata\system\strings.txt /YS
